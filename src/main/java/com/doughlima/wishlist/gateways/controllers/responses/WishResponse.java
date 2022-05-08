3e07aeadda64f336ad2036caaef48b16c72826dc
DouglasHLima <douglashdelima@gmail.com>
2022-05-08 09:05:07 -0300

    refactoring: imports and named relation

--- src/main/java/com/doughlima/wishlist/gateways/controllers/responses/WishResponse.java
+++ src/main/java/com/doughlima/wishlist/gateways/controllers/responses/WishResponse.java
@@ -1,24 +1,23 @@
 package com.doughlima.wishlist.gateways.controllers.responses;
 
 import com.doughlima.wishlist.domains.Wish;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Getter @Setter
+@Relation(value = "wish", collectionRelation = "wishes")
 public class WishResponse extends RepresentationModel<WishResponse> {
 
-    private UUID id;
-    private UUID user;
     private UUID product;
     private LocalDate createdAt;
 
     public WishResponse(Wish wish) {
-        this.id = wish.getId();
-        this.user = wish.getUser();
         this.product = wish.getProduct();
         this.createdAt = wish.getCreated();
     }
