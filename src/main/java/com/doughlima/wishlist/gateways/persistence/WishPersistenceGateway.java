2158b668a5722aded33dfb2a9dc983c410ca0fa3
DouglasHLima <douglashdelima@gmail.com>
2022-05-08 09:06:21 -0300

    feat: create get all and exists by id

--- src/main/java/com/doughlima/wishlist/gateways/persistence/WishPersistenceGateway.java
+++ src/main/java/com/doughlima/wishlist/gateways/persistence/WishPersistenceGateway.java
@@ -2,8 +2,14 @@ package com.doughlima.wishlist.gateways.persistence;
 
 import com.doughlima.wishlist.domains.Wish;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface WishPersistenceGateway {
 
     Wish save(Wish wish);
 
+    List<Wish> getAll(UUID userId);
+
+    boolean existsById(UUID userId, UUID productId);
 }
--- src/main/java/com/doughlima/wishlist/gateways/persistence/impl/mongo/WishPersistanceGatewayImpl.java
+++ src/main/java/com/doughlima/wishlist/gateways/persistence/impl/mongo/WishPersistanceGatewayImpl.java
@@ -4,11 +4,27 @@ import com.doughlima.wishlist.domains.Wish;
 import com.doughlima.wishlist.gateways.persistence.WishPersistenceGateway;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 @Component
 public class WishPersistanceGatewayImpl implements WishPersistenceGateway {
 
+    private final List<Wish> wishes = new ArrayList<>();
     @Override
     public Wish save(Wish wish) {
+        wishes.add(wish);
         return wish;
+    }
+
+    @Override
+    public List<Wish> getAll(UUID userId) {
+        return wishes;
+    }
+
+    @Override
+    public boolean existsById(UUID userId, UUID productId) {
+        return wishes.stream().anyMatch((wish -> wish.getProduct().equals(productId) && wish.getUser().equals(userId)));
     }
 }
